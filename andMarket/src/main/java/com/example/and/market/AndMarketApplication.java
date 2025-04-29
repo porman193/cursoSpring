07bbc08ae6585e739f14bc62f0c2c0720fba12f0
porman193 <porman193@gmail.com>
2025-04-28 21:42:54 -0500

    diferetes perfile

---
+++ andMarket/src/main/java/com/example/and/market/AndMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.and.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndMarketApplication.class, args);
+	}
+
+}
---
+++ andMarket/src/main/java/com/example/and/market/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.and.market;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping("/hi")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
